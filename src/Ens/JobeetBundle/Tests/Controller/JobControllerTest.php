a0a20e2c7f76b8b43d71621e2e6c5f83a3ecf5d8
Konstantin Sherpaev <konstantin.sherpaev@gmail.com>
2013-10-08 17:53:11 +0700

    Day 11, Back to the Future in a Test

--- src/Ens/JobeetBundle/Tests/Controller/JobControllerTest.php
+++ src/Ens/JobeetBundle/Tests/Controller/JobControllerTest.php
@@ -4,31 +4,6 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class JobControllerTest extends WebTestCase
 {
-    public function getMostRecentProgrammingJob()
-    {
-        $kernel = static::createKernel();
-        $kernel->boot();
-        $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
-
-        $query = $em->createQuery('SELECT j from EnsJobeetBundle:Job j LEFT JOIN j.category c WHERE c.slug = :slug AND j.expires_at > :date ORDER BY j.created_at DESC');
-        $query->setParameter('slug', 'programming');
-        $query->setParameter('date', date('Y-m-d H:i:s', time()));
-        $query->setMaxResults(1);
-        return $query->getSingleResult();
-    }
-
-    public function getExpiredJob()
-    {
-        $kernel = static::createKernel();
-        $kernel->boot();
-        $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
-
-        $query = $em->createQuery('SELECT j from EnsJobeetBundle:Job j WHERE j.expires_at < :date');
-        $query->setParameter('date', date('Y-m-d H:i:s', time()));
-        $query->setMaxResults(1);
-        return $query->getSingleResult();
-    }
-
     public function testIndex()
     {
         // get the custom parameters from app config.yml
@@ -125,5 +100,139 @@ class JobControllerTest extends WebTestCase
         $this->assertTrue($crawler->filter('#job_how_to_apply')->siblings()->first()->filter('.error_list')->count() == 1);
         // check if we have error on job_email field
         $this->assertTrue($crawler->filter('#job_email')->siblings()->first()->filter('.error_list')->count() == 1);
+    }
+
+    public function testPublishJob()
+    {
+        $client = $this->createJob(array('job[position]' => 'FOO1'));
+        $crawler = $client->getCrawler();
+        $form = $crawler->selectButton('Publish')->form();
+        $client->submit($form);
+
+        $kernel = static::createKernel();
+        $kernel->boot();
+        $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+
+        $query = $em->createQuery('SELECT count(j.id) from EnsJobeetBundle:Job j WHERE j.position = :position AND j.is_activated = 1');
+        $query->setParameter('position', 'FOO1');
+        $this->assertTrue(0 < $query->getSingleScalarResult());
+    }
+
+    public function testEditJob()
+    {
+        $client = $this->createJob(array('job[position]' => 'FOO3'), true);
+        $crawler = $client->getCrawler();
+        $crawler = $client->request('GET', sprintf('/job/%s/edit', $this->getJobByPosition('FOO3')->getToken()));
+        $this->assertTrue(404 === $client->getResponse()->getStatusCode());
+    }
+
+    public function testDeleteJob()
+    {
+        $client = $this->createJob(array('job[position]' => 'FOO2'));
+        $crawler = $client->getCrawler();
+        $form = $crawler->selectButton('Delete')->form();
+        $client->submit($form);
+
+        $kernel = static::createKernel();
+        $kernel->boot();
+        $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+
+        $query = $em->createQuery('SELECT count(j.id) from EnsJobeetBundle:Job j WHERE j.position = :position');
+        $query->setParameter('position', 'FOO2');
+        $this->assertTrue(0 == $query->getSingleScalarResult());
+    }
+
+    public function testExtendJob()
+    {
+        // A job validity cannot be extended before the job expires soon
+        $client = $this->createJob(array('job[position]' => 'FOO4'), true);
+        $crawler = $client->getCrawler();
+        $this->assertTrue($crawler->filter('input[type=submit]:contains("Extend")')->count() == 0);
+
+        // A job validity can be extended when the job expires soon
+
+        // Create a new FOO5 job
+        $client = $this->createJob(array('job[position]' => 'FOO5'), true);
+        // Get the job and change the expire date to today
+        $kernel = static::createKernel();
+        $kernel->boot();
+        $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+        $job = $em->getRepository('EnsJobeetBundle:Job')->findOneByPosition('FOO5');
+        $job->setExpiresAt(new \DateTime());
+        $em->flush();
+        // Go to the preview page and extend the job
+        $crawler = $client->request('GET', sprintf('/job/%s/%s/%s/%s', $job->getCompanySlug(), $job->getLocationSlug(), $job->getToken(), $job->getPositionSlug()));
+        $crawler = $client->getCrawler();
+        $form = $crawler->selectButton('Extend')->form();
+        $client->submit($form);
+        // Reload the job from db
+        $job = $this->getJobByPosition('FOO5');
+        // Check the expiration date
+        $this->assertTrue($job->getExpiresAt()->format('y/m/d') == date('y/m/d', time() + 86400 * 30));
+    }
+
+    public function getMostRecentProgrammingJob()
+    {
+        $kernel = static::createKernel();
+        $kernel->boot();
+        $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+
+        $query = $em->createQuery('SELECT j from EnsJobeetBundle:Job j LEFT JOIN j.category c WHERE c.slug = :slug AND j.expires_at > :date ORDER BY j.created_at DESC');
+        $query->setParameter('slug', 'programming');
+        $query->setParameter('date', date('Y-m-d H:i:s', time()));
+        $query->setMaxResults(1);
+        return $query->getSingleResult();
+    }
+
+    public function getExpiredJob()
+    {
+        $kernel = static::createKernel();
+        $kernel->boot();
+        $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+
+        $query = $em->createQuery('SELECT j from EnsJobeetBundle:Job j WHERE j.expires_at < :date');
+        $query->setParameter('date', date('Y-m-d H:i:s', time()));
+        $query->setMaxResults(1);
+        return $query->getSingleResult();
+    }
+
+    public function createJob($values = array(), $publish = false)
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/job/new');
+        $form = $crawler->selectButton('Preview your job')->form(array_merge(array(
+            'job[company]'      => 'Sensio Labs',
+            'job[url]'          => 'http://www.sensio.com/',
+            'job[position]'     => 'Developer',
+            'job[location]'     => 'Atlanta, USA',
+            'job[description]'  => 'You will work with symfony to develop websites for our customers.',
+            'job[how_to_apply]' => 'Send me an email',
+            'job[email]'        => 'for.a.job@example.com',
+            'job[is_public]'    => false,
+        ), $values));
+
+        $client->submit($form);
+        $client->followRedirect();
+
+        if($publish) {
+            $crawler = $client->getCrawler();
+            $form = $crawler->selectButton('Publish')->form();
+            $client->submit($form);
+            $client->followRedirect();
+        }
+
+        return $client;
+    }
+
+    public function getJobByPosition($position)
+    {
+        $kernel = static::createKernel();
+        $kernel->boot();
+        $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+
+        $query = $em->createQuery('SELECT j from EnsJobeetBundle:Job j WHERE j.position = :position');
+        $query->setParameter('position', $position);
+        $query->setMaxResults(1);
+        return $query->getSingleResult();
     }
 }
