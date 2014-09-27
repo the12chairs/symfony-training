2bcdd108dd123d151293db138c088bc48da6fc50
Maksim Bondarenko <mbondarenko92@gmail.com>
2014-09-27 14:42:42 +0700

    Add user admin

---
+++ src/Ens/JobeetBundle/Admin/UserAdmin.php
@@ -0,0 +1,40 @@
+<?php
+namespace Ens\JobeetBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Validator\ErrorElement;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class UserAdmin extends Admin
+{
+    // setup the default sort column and order
+    protected $datagridValues = array(
+        '_sort_order' => 'ASC',
+        '_sort_by' => 'id'
+    );
+
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('username')
+            ->add('password')
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('username')
+        ;
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('id')
+            ->add('username')
+        ;
+    }
+}
